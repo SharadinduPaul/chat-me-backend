b6e8b43c15088ca1ca38f1f68f756b2aa5a90417
SharadinduPaul <sharadindu.paul007@gmail.com>
2023-02-20 08:59:17 +0530

    mongoose error fixed - node version 2.. or later

--- config/db.js
+++ config/db.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     mongoose.set("strictQuery", false);
+//     mongoose.connect(process.env.MONGO_URI);
+
+//     console.log("mongodb successfully connected");
+//   } catch (err) {
+//     console.log("failed to connect to MongoDB");
+//     console.log(err);
+//   }
+// };
+
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI);
-
-    console.log("mongodb successfully connected");
+    await mongoose.set("strictQuery", false);
+    await mongoose.set("bufferCommands", false);
+    await mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      },
+      () => {
+        console.log("Connected to DB");
+      }
+    );
+    console.log("inside mongoose connect try block")
   } catch (err) {
-    console.log("failed to connect to MongoDB");
-    console.log(err);
+    console.error(err);
   }
 };
 
