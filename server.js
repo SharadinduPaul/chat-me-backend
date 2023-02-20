14c0f952218008ee56143b0288e1c2686c6c26c2
SharadinduPaul <sharadindu.paul007@gmail.com>
2023-02-20 10:02:41 +0530

    cors list updated 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://web-chatme.netlify.app/"],
+    origin: ["http://localhost:3000", "https://web-chatme.netlify.app"],
   })
 );
 
@@ -24,7 +24,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:3000", "https://web-chatme.netlify.app/"],
+    origin: ["http://localhost:3000", "https://web-chatme.netlify.app"],
   },
 });
 
