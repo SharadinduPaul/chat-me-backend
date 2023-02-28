7f38ef5a7f23bf8099cf97ac200b756d835c68f5
arpansadhu10 <74260413+arpansadhu10@users.noreply.github.com>
2023-02-28 14:17:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
 });
